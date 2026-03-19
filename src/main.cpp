8afa632efbbe0f97fa5ecf8a4a585032e69e2a5a
pengdefeng <2450410477@qq.com>
2026-03-19 23:27:35 +0800

    增加密码进入开发者模式，并退出全屏

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
-#include <QApplication>
 #include "mainwindow.h"
+
+#include <QApplication>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-
-    MainWindow window;
-    window.show();
-
-    return app.exec();
+    QApplication a(argc, argv);
+    
+    // 设置应用程序信息
+    QApplication::setApplicationName("PotBot HMI");
+    QApplication::setApplicationVersion("2.1.0");
+    QApplication::setOrganizationName("Peng Defeng");
+    
+    // 创建主窗口
+    MainWindow w;
+    
+    // 显示主窗口（会自动全屏）
+    w.show();
+    
+    return a.exec();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,12 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
-namespace Ui
-{
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -18,23 +17,63 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    // 公有更新函数
     void updateYawAngle(double angle);
     void updateBattery(int value);
     void updateLatitude(double lat);
     void updateLongitude(double lon);
+    bool isFullScreenMode() const { return fullScreenMode; }
+
+protected:
+    void showEvent(QShowEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
 
 private slots:
-
-    // 开始运行按钮的槽函数
+    // 按钮槽函数
     void on_StartLabel_clicked();
-    // 设置按钮的槽函数
     void on_SettingLabel_clicked();
+    void on_CloseSettingsBtn_clicked();
+    void on_DevModeBtn_clicked();
+    
+    // 数字键盘按钮
+    void on_num1_clicked();
+    void on_num2_clicked();
+    void on_num3_clicked();
+    void on_num4_clicked();
+    void on_num5_clicked();
+    void on_num6_clicked();
+    void on_num7_clicked();
+    void on_num8_clicked();
+    void on_num9_clicked();
+    void on_num0_clicked();
+    void on_clearBtn_clicked();
+    void on_backspaceBtn_clicked();
+    void on_confirmPasswordBtn_clicked();
+    void on_cancelPasswordBtn_clicked();
+    
+    // 堆叠窗口切换信号
+    void on_stackedMainArea_currentChanged(int index);
+    
+    // 标签按钮组
+    void on_TabSystemInfo_clicked();
+    void on_TabParams_clicked();
+    void on_TabDeveloper_clicked();
+    void on_TabLog_clicked();
+    void on_TabFirmware_clicked();
 
 private:
-
     Ui::MainWindow *ui;
-
     bool robotRunning;
+    bool fullScreenMode;
+    QString passwordInput;
+    QString correctPassword;
+    
+    void setupDeveloperFeatures();
+    void updateTabSelection(QPushButton* selectedTab);
+    void showPasswordDialog();
+    void hidePasswordDialog();
+    void exitFullScreen();
+    void adjustFontSizes();
 };
 
-#endif
+#endif // MAINWINDOW_H
